4b81fd5f3502ccc3718916d236ec76c5b5cbd159
omnia <omniaosman22019@ci.suez.edu.eggit config --global user.email omniaosman22019@ci.suez.edu.eg>
2021-01-13 20:43:13 +0200

    second section in page

---
+++ cafe.js
@@ -0,0 +1,16 @@
+// shown navBar when scroll
+
+// Sticky NAVBAR
+// window.addEventListener('scroll', function(){
+//      let header = document.querySelector('#header');
+//      header.classList.toggle('sticky', window.scrollY   > 0)
+//    });
+
+//    // Toggle icon
+//    function toggleIcon(){
+//      let toggle = document.querySelector('#nav-toggle');
+//      let list = document.querySelector('#nav-list')
+
+//      toggle.classList.toggle('toggled');
+//      list.classList.toggle('show-menu')
+//    };
